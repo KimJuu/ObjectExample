0c197d390e9c20206ed0b8cf845d57ca38d1cbb4
Kimjuu <point8622@gmail.com>
2022-12-19 23:18:26 +0900

    자율성을 보장하기위해 모든 객체에 응집도를 높였지만, 결국 의존성이 추가되는 상황이 발생 트레이드오프 상황을 어떻게 유연하게 처리할지 고민해보자

--- src/main/java/chapter01/example01/Bag.java
+++ src/main/java/chapter01/example01/Bag.java
@@ -14,25 +14,35 @@ public class Bag {
         this.amount = amount;
     }
 
-    public boolean hasInvitation(){
+    private boolean hasInvitation(){
         return invitation != null;
     }
 
-    public void hasTicket(Ticket ticket){
+    private void hasTicket(Ticket ticket){
         this.ticket = ticket;
     }
 
-    public void setTicket(Ticket ticket){
+    private void setTicket(Ticket ticket){
        this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount){
+    private void minusAmount(Long amount){
         this.amount -=amount;
     }
 
-    public void plusAmount(Long amount){
+    private void plusAmount(Long amount){
         this.amount += amount;
     }
 
+    public Long hold(Ticket ticket){
+        if(hasInvitation()){
+            setTicket(ticket);
+            return 0L;
+        }else{
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
 
 }
